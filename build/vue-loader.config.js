02971e2865aa78f145b507c3f2a4c97553e52813
Lac <laclys@126.com>
2018-04-08 00:23:00 +0800

    vuew-loader use css moudles

--- build/vue-loader.config.js
+++ build/vue-loader.config.js
@@ -2,6 +2,9 @@ module.exports = (isDev) => {
   return {
     preserveWhitespace: true, // 处理空白区域
     extractCSS: !isDev, // 将vue文件中的css样式拿出来
-    cssModules: {}
+    cssModules: {
+      localIdentName: isDev ? '[path]-[name]-[hash:base64:5]' : '[hash:base64:5]',
+      camelCase: true
+    }
   }
 }
