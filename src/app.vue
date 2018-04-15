54cd33357fdbd2795a2734ded5db32eb6414150e
Lac <laclys@126.com>
2018-04-15 14:47:37 +0800

    router config

--- src/app.vue
+++ src/app.vue
@@ -2,7 +2,8 @@
   <div id="app" >
     <div id="cover" ></div>
     <Header></Header>
-    <Todo></Todo>
+    <!-- <Todo></Todo> -->
+    <router-view />
     <Footer></Footer>
   </div>
 </template>
