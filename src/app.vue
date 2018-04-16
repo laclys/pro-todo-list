7c5e85a3299ff73aee47f5bd4cb387852d0058d9
Lac <laclys@126.com>
2018-04-17 00:28:17 +0800

    add Vuex

--- src/app.vue
+++ src/app.vue
@@ -2,7 +2,8 @@
   <div id="app" >
     <div id="cover" ></div>
     <Header></Header>
-    <router-link to="/app/123" >app</router-link>
+    <p>{{count}}</p>
+    <router-link to="/app" >app</router-link>
     <router-link to="/login" >login</router-link>
     <!-- <Todo></Todo> -->
     <transition name="fade">
@@ -23,7 +24,17 @@ export default {
     // Todo
   },
   mounted () {
-    console.log(this.$route)
+    // console.log(this.$route)
+    console.log(this.$store)
+    let i = 1
+    setInterval(() => {
+      this.$store.commit('updateCount', i++)
+    }, 1000)
+  },
+  computed: {
+    count () {
+      return this.$store.state.count
+    }
   }
 }
 </script>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import App from './app.vue'
 import VueRouter from 'vue-router'
+import createStore from './store/store'
+import Vuex from 'vuex'
 
 import './assets/styles/global.styl'
 
 import createRouter from './config/router'
 
+Vue.use(Vuex)
 Vue.use(VueRouter)
 
 const router = createRouter()
-
+const store = createStore()
 // 导航守卫
 router.beforeEach((to, from, next) => {
   console.log('before each invoked')
@@ -27,5 +30,6 @@ router.afterEach((to, from) => {
 
 new Vue({
   router,
+  store,
   render: (h) => h(App)
 }).$mount('#root')
