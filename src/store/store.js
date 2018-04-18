db0e36e2504ba6b3e040321908e0b5bf0d858f09
Lac <laclys@126.com>
2018-04-18 17:10:16 +0800

    vuex add hotUpdate

--- src/store/store.js
+++ src/store/store.js
@@ -9,10 +9,34 @@ import getters from './getters/getters'
 import actions from './actions/actions'
 
 export default () => {
-  return new Vuex.Store({
+  const store = new Vuex.Store({
     state: defaultState,
     mutations,
     getters,
     actions
   })
+
+  if (module.hot) {
+    module.hot.accept([
+      './state/state',
+      './mutations/mutations',
+      './getters/getters',
+      './actions/actions'
+    ], () => {
+      const newState = require('./state/state').default
+      const newMutations = require('./mutations/mutations').default
+      const newGetters = require('./getters/getters').default
+      const newActions = require('./actions/actions').default
+
+      store.hotUpdate({
+        state: newState,
+        mutations: newMutations,
+        getters: newGetters,
+        actions: newActions
+      })
+    })
+  }
+
+  return store
+
 }
